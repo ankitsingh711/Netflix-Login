9ce1e2c0912c8cce50e31abf009e90f460a08d06
ankit711 <itsankitsingh711@edureka.co>
2022-04-17 14:00:50 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const port = 2000;
-const MongoLiveUrl = "mongodb+srv://login:test12345@netflixlogin.2gsnq.mongodb.net/NetflixLogin?retryWrites=true&w=majority";
+const MongoLiveUrl = "mongodb+srv://login:test12345@netflixlogin.2gsnq.mongodb.net/netflixdatalogin?retryWrites=true&w=majority";
 const cors = require('cors');
 app.use(cors());
 
